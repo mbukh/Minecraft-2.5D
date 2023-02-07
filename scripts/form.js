61ca42abeb03f6e808406c6a9c91e0129992d223
mbukhman <mbukhman@gmail.com>
2023-02-08 02:03:08 +0400

    parallax to background

--- scripts/form.js
+++ scripts/form.js
@@ -1,37 +1,50 @@
+const background = document.querySelector(".landing");
 const panel = document.querySelector(".panel");
 const form = document.querySelector("form");
-const maxRotateX = 30;
-const maxRotateY = 30;
+const maxRotateX = 15;
+const maxRotateY = 15;
 const elementsToMove = Array.from(panel.children);
 
-document.body.addEventListener("mousemove", (event) => {
-    createParallax(event, panel);
+document.body.addEventListener("mousemove", (e) => {
+    createParallax(e);
+    shiftBackground(e);
 });
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     const nickName = form.querySelector("#nickname").value;
-    const email = form.querySelector("#email").value;
+    // const email = form.querySelector("#email").value;
     if (nickName.length >= 3) {
         window.localStorage.setItem("nickname", nickName);
-        window.localStorage.setItem("email", email);
+        // window.localStorage.setItem("email", email);
         window.location.href = "./game.html";
     }
 });
 
-function createParallax(event, element) {
+function createParallax(e) {
     // Original idea and implementation
     // https://codepen.io/rdfriedl/pen/obNpeV
-    var cx = element.offsetLeft;
-    var cy = element.offsetTop;
+    var cx = panel.offsetLeft;
+    var cy = panel.offsetTop;
     var rotateX =
-        ((event.pageY - cy) / Math.max(cy, document.body.offsetHeight - cy)) *
+        ((e.pageY - cy) / Math.max(cy, document.body.offsetHeight - cy)) *
         maxRotateY;
     var rotateY =
-        ((event.pageX - cx) / Math.max(cx, document.body.offsetWidth - cx)) *
+        ((e.pageX - cx) / Math.max(cx, document.body.offsetWidth - cx)) *
         maxRotateX;
-    element.style.cssText = `transform:
+    panel.style.cssText = `transform:
         translate(-50%,-50%)
         rotateX(${-rotateX}deg)
         rotateY(${rotateY}deg)`;
+}
+
+function shiftBackground(e) {
+    background.style.setProperty(
+        "--offsetX",
+        -(e.pageX - window.innerWidth / 2) / 50 + "px"
+    );
+    background.style.setProperty(
+        "--offsetY",
+        (e.pageY - window.innerHeight / 2) / 50 + "px"
+    );
 }
